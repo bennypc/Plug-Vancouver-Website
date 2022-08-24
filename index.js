eedf548c84aec87005b72457bd67ac289cf522fd
bennypc <benny.pincha@gmail.com>
2022-08-24 01:17:52 -0700

    fixed instagram alignment

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var counter = 1;
 setInterval(function(){
   document.getElementById('radio' + counter).checked = true;
   counter++;
-  if(counter > 4){
+  if(counter > 5){
     counter = 1;
   }
 }, 3000);
