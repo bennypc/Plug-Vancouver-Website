bb1863569c20f2a40a2658918730fb5bc482c235
bennypc <benny.pincha@gmail.com>
2022-08-23 22:45:06 -0700

    added button tabs

---
+++ index.js
@@ -0,0 +1,25 @@
+function openPage(pageName, elmnt) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tabButtons;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the background color of all tablinks/buttons
+    tabButtons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButtons.length; i++) {
+        tabButtons[i].style.backgroundColor = "";
+        tabButtons[i].style.color = "#FFFFFF";
+    }
+
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = "#FFFFFF";
+    elmnt.style.color = "#000000"
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
