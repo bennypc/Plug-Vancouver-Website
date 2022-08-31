b6420b3f49c2a002127353b78d4c85a1c820f540
bennypc <benny.pincha@gmail.com>
2022-08-30 17:33:10 -0700

    installed node and eslint

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function openPage(pageName, elmnt) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tabButtons;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tabButtons;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
 
-    // Remove the background color of all tablinks/buttons
-    tabButtons = document.getElementsByClassName("tab-button");
-    for (i = 0; i < tabButtons.length; i++) {
-        tabButtons[i].style.backgroundColor = "";
-        tabButtons[i].style.color = "#FFFFFF";
-    }
+  // Remove the background color of all tablinks/buttons
+  tabButtons = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].style.backgroundColor = "";
+    tabButtons[i].style.color = "#FFFFFF";
+  }
 
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
 
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = "#FFFFFF";
-    elmnt.style.color = "#000000"
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = "#FFFFFF";
+  elmnt.style.color = "#000000"
 }
 
 // Get the element with id="defaultOpen" and click on it
@@ -26,10 +26,10 @@ document.getElementById("defaultOpen").click();
 
 
 var counter = 1;
-setInterval(function(){
+setInterval(function () {
   document.getElementById('radio' + counter).checked = true;
   counter++;
-  if(counter > 5){
+  if (counter > 5) {
     counter = 1;
   }
 }, 3000);
